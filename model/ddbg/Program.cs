466b10692339ace42c5971f05712ba3cb2098d43
hdpotter <hdpotter@iastate.edu>
2021-12-05 10:34:24 -0600

    hello world

---
+++ model/ddbg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ddbg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
